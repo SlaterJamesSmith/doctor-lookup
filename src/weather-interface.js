1a18c8d1a6f9213eefb08eb56c4dd1e57d4ab430
Slater Smith <smithslater@gmail.com>
2019-03-20 13:44:46 -0700

    create output messages and javascript conditional functions for rain, snow, cloudy and clear api returns

--- src/weather-interface.js
+++ src/weather-interface.js
@@ -6,6 +6,19 @@ $(document).ready(function() {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`)
       $('.showErrors').text("");
+      const weatherObj = response.weather[0];
+      // $('.animation').text(`${weatherObj.description}`)
+      if (response.weather[0].main === 'Clear') {
+        $('.animation').text("clear clear sky sky");
+      } else if (response.weather[0].main === 'Clouds') {
+        $('.animation').text("cloudy");
+      } else if (response.weather[0].main === 'Snow') {
+        $('.animation').text("snow");
+      } else if (response.weather[0].main === 'Rain') {
+        $('.animation').text("rain");
+      } else {
+        $('.animation').text("");
+      }
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
