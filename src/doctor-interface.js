14c1a60ccf6c05bb6a1fd99e74392d6d011bdb50
Slater Smith <smithslater@gmail.com>
2019-03-22 08:44:34 -0700

    clone template project, make new .env file with new API reference variable

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`).then(function(response) {
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.exports.apiKey}`).then(function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`)
       $('.showErrors').text("");
