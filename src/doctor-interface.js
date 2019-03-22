7dcd347ea8ede282746df1b9afc6e662d2f9d33f
Slater Smith <smithslater@gmail.com>
2019-03-22 10:05:39 -0700

    include correct api url address in code

--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -2,23 +2,25 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.exports.apiKey}`).then(function(response) {
+    $.get(https:`https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.exports.apiKey}`
+
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.exports.apiKey}`).then(function(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
       $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`)
       $('.showErrors').text("");
-      const weatherObj = response.weather[0];
-      // $('.animation').text(`${weatherObj.description}`)
-      if (response.weather[0].main === 'Clear') {
-        $('.animation').text("clear clear sky sky");
-      } else if (response.weather[0].main === 'Clouds') {
-        $('.animation').text("cloudy");
-      } else if (response.weather[0].main === 'Snow') {
-        $('.animation').text("snow");
-      } else if (response.weather[0].main === 'Rain') {
-        $('.animation').text("rain");
-      } else {
-        $('.animation').text("");
-      }
+      // const weatherObj = response.weather[0];
+      // // $('.animation').text(`${weatherObj.description}`)
+      // if (response.weather[0].main === 'Clear') {
+      //   $('.animation').text("clear clear sky sky");
+      // } else if (response.weather[0].main === 'Clouds') {
+      //   $('.animation').text("cloudy");
+      // } else if (response.weather[0].main === 'Snow') {
+      //   $('.animation').text("snow");
+      // } else if (response.weather[0].main === 'Rain') {
+      //   $('.animation').text("rain");
+      // } else {
+      //   $('.animation').text("");
+      // }
 
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
