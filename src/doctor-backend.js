0d951d54227bd706ca6b4a87d21f7da83f754148
Slater Smith <smithslater@gmail.com>
2019-03-22 19:01:28 -0700

    create doctor address and availability information outputs, as well as UI elements for searching by doctor name

--- src/doctor-backend.js
+++ src/doctor-backend.js
@@ -16,14 +16,34 @@ export class SymptomQuery {
     });
 
   }
+
+  getDoctorByName(name) {
+    let Promise = require("es6-promise").Promise;
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&user_location=45.5122%2C%20-122.6587&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+
+  }
+
   getDoctorList(response) {
     let body = JSON.parse(response);
     let doctors = body.data;
     let doctorsArry = [];
     doctors.forEach(function(doctor) {
       doctorsArry.push(`${doctor.profile.first_name} ${doctor.profile.last_name}`);
-      console.log(doctorsArry);
     });
     return doctorsArry;
   }
+
+
 }
--- src/doctor-interface.js
+++ src/doctor-interface.js
@@ -1,43 +1,92 @@
 import { SymptomQuery } from "../src/doctor-backend.js";
 
 $(document).ready(function() {
+
+  $('#doctorRefresh').click(function() {
+    console.log("hi")
+    location.reload(true);
+  });
+
   $('#doctorSymptom').click(function() {
+    // $('.showSymptom').text("new search");
     let symptom = $('#symptom').val();
     $('#symptom').val("");
+    $('#doctorRefresh').show()
+    $('.startingButtons').hide();
 
 
     let symptomQuery = new SymptomQuery();  // create instance of SymptomQuery class
     let promise = symptomQuery.getDoctorBySymptom(symptom);  // call the instance method and pass in user input
 
     promise.then(function(response) {
-      // let body = JSON.parse(response);
-      // // let name = `${doctors[0].profile.first_name} ${doctors[0].profile.last_name}`
-      // // console.log(doctors[0].profile.first_name);
-      // // console.log(name);
-      // let doctors = body.data;
-      // let doctorsArry = [];
-      // doctors.forEach(function(doctor) {
-      //   doctorsArry.push(`${doctor.profile.first_name} ${doctor.profile.last_name}`);
-      //   console.log(doctorsArry);
-      // });
       let names = symptomQuery.getDoctorList(response);
-      let docObj = JSON.parse(response)
-      names.forEach(function(name, index) {
-        let phones = (docObj.data[index].practices[0].phones);
-        let phoneList = [];
-        phones.forEach(function(phone) {
-          phoneList.push(phone["type"]+ ":");
-          phoneList.push(phone["number"]);
-        });
-        console.log(phoneList);
-        let phoneStr = phoneList.join(" ");
-        $('.showSymptom').append(`<ol>${name}</ol><li>${phoneStr}</li>`)
-        console.log(docObj.data[index].practices[0].phones);
-      })
-      // $('.showSymptom').text(`Doctors who can help you with ${symptom} in Portland: ${doctorsArryNew.join(", ")}`);
-
+      if (names[0] === undefined) {
+        $('.showSymptom').text("no matches")
+      } else {
+        let docObj = JSON.parse(response)
+        names.forEach(function(name, index) {
+          let phones = (docObj.data[index].practices[0].phones);
+          let phoneList = [];
+          phones.forEach(function(phone) {
+            phoneList.push(phone["type"]+ ":");
+            phoneList.push(phone["number"]);
+          });
+          let phoneStr = phoneList.join(" ");
+          let practices = docObj.data[index].practices[0];
+          let website = practices["website"];
+          let address = practices["visit_address"].street;
+          let patients = practices.accepts_new_patients
+          let infos = [address, phoneStr, website, patients];
+          // console.log(practices["website"]);
+          $('.showSymptom').append(`<ol>${name}</ol><li>address: ${infos[0]}</li><li>${infos[1]}</li><li>website: ${infos[2]}</li><li>${infos[3]}</li>`);
+          document.body.innerHTML = document.body.innerHTML.replace('undefined', 'none')
+          document.body.innerHTML = document.body.innerHTML.replace('true', 'Curretnly accepting patients')
+          document.body.innerHTML = document.body.innerHTML.replace('false', 'Not accepting patients')
+        })
+      }
     }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+      $('.showSymptom').text(`There was an error processing your request: ${error.message}`);
+    });
+  });
+
+  $('#doctorName').click(function() {
+    // $('.showSymptom').text("new search");
+    let name = $('#name').val();
+    $('#name').val("");
+    $('#doctorRefresh').show()
+    $('.startingButtons').hide();
+
+    let symptomQuery = new SymptomQuery();  // create instance of SymptomQuery class
+    let promise = symptomQuery.getDoctorByName(name);  // call the instance method and pass in user input
+
+    promise.then(function(response) {
+      let names = symptomQuery.getDoctorList(response);
+      if (names[0] === undefined) {
+        $('.showSymptom').text("no matches")
+      } else {
+        let docObj = JSON.parse(response)
+        names.forEach(function(name, index) {
+          let phones = (docObj.data[index].practices[0].phones);
+          let phoneList = [];
+          phones.forEach(function(phone) {
+            phoneList.push(phone["type"]+ ":");
+            phoneList.push(phone["number"]);
+          });
+          let phoneStr = phoneList.join(" ");
+          let practices = docObj.data[index].practices[0];
+          let website = practices["website"];
+          let address = practices["visit_address"].street;
+          let patients = practices.accepts_new_patients
+          let infos = [address, phoneStr, website, patients];
+          // console.log(practices["website"]);
+          $('.showSymptom').append(`<ol>${name}</ol><li>address: ${infos[0]}</li><li>${infos[1]}</li><li>website: ${infos[2]}</li><li>${infos[3]}</li>`);
+          document.body.innerHTML = document.body.innerHTML.replace('undefined', 'none')
+          document.body.innerHTML = document.body.innerHTML.replace('true', 'Curretnly accepting patients')
+          document.body.innerHTML = document.body.innerHTML.replace('false', 'Not accepting patients')
+        });
+      }
+    }, function(error) {
+      $('.showSymptom').text(`There was an error processing your request: ${error.message}`);
     });
   });
 
